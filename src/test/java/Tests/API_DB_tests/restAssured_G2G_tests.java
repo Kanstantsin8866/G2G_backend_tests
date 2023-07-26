9a7108f0e9e0f969a0df68741fc2b021f65205dd
Kanstantsin8866 <Kanstantsin@gym2gym.com>
2023-07-26 14:31:47 -0500

    "updated query for GET requests in Rest ASSURED"

--- src/test/java/Tests/API_DB_tests/restAssured_G2G_tests.java
+++ src/test/java/Tests/API_DB_tests/restAssured_G2G_tests.java
@@ -1,7 +1,9 @@
 package Tests.API_DB_tests;
-import org.testng.Assert;
+
+import io.restassured.response.ResponseBody;
+//import org.testng.Assert;
 import io.restassured.RestAssured;
-import  io.restassured.response.Response;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
@@ -12,6 +14,7 @@ import static io.restassured.RestAssured.*;
 
 public class restAssured_G2G_tests {
 
+    //initial GET request test for 'userId' column
     @Test
     public void restGet_test() throws InterruptedException {
 
@@ -23,37 +26,42 @@ public class restAssured_G2G_tests {
         List<Integer> code_5xx = new ArrayList<>();
         int id;
 
+        //looping through 100 users in our psqlDB by invoking Node JS server
         for (id = 0; id < 100; id++) {
-            int statusCode = 0;
-            try {
-                RequestSpecification httpRequest = RestAssured.given();
-                Response response = httpRequest.get("/user/v1/" + id);
-                statusCode = response.getStatusCode();
-                Assert.assertEquals(statusCode, 200);
+            int statusCode;
+            RequestSpecification httpRequest = RestAssured.given();
+            Response response = httpRequest.get("/user/v1/" + id);
+            ResponseBody body = response.getBody();
+
+            statusCode = response.getStatusCode();
+
+            Thread.sleep(1000);
+            if (statusCode > 299) {
+
+                System.out.println("This id-> " + id + " has Code status: " + statusCode + "'" + body.asString() + "'");
+                codeNumbers.add(statusCode);
+            } else {
                 System.out.println("This id -> " + id + " has code:" + statusCode);
                 codeNumbers.add(statusCode);
-            } catch (AssertionError err) {
 
-                System.out.println("This nonexisting id-> " + id + " has Code status: " + statusCode);
-                codeNumbers.add(statusCode);
-                Thread.sleep(1000);
             }
         }
         System.out.println("FINAL Statuscode's log: " + codeNumbers);
 
+        //sorting status CODES by their numbers in an array
         for (Integer codeNumber : codeNumbers)
             if (codeNumber < 299)
                 code_2xx.add(codeNumber);
-            else if(codeNumber > 299 && codeNumber < 399)
+            else if (codeNumber > 299 && codeNumber < 399)
                 code_3xx.add(codeNumber);
-            else if(codeNumber > 399 && codeNumber <499)
+            else if (codeNumber > 399 && codeNumber < 499)
                 code_4xx.add(codeNumber);
-            else if(codeNumber > 499)
+            else if (codeNumber > 499)
                 code_5xx.add(codeNumber);
-        System.out.println("Code  2xx : "+ code_2xx.size()+" count");
-        System.out.println("Code 3xx: "+ code_3xx.size()+" count");
-        System.out.println("Code 4xx: "+ code_4xx.size()+" count");
-        System.out.println("Code 5xx: "+ code_5xx.size()+" count");
+        System.out.println("Code  2xx : " + code_2xx.size() + " count");
+        System.out.println("Code 3xx: " + code_3xx.size() + " count");
+        System.out.println("Code 4xx: " + code_4xx.size() + " count");
+        System.out.println("Code 5xx: " + code_5xx.size() + " count");
 
     }
 }
